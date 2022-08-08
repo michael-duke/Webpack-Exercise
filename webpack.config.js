dc2edfbcee0c7bf7263798838c45a28477c52ff4
michael-duke <michael.21duke@gmail.com>
2022-08-09 00:26:55 +0300

    Use HTML plugin and cache busting

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,17 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
   entry: './src/index.js',
   output: {
-    filename: 'main.js',
+    filename: "main.[contenthash].js",
     path: path.resolve(__dirname, 'dist'),
   },
+  plugins: [new HtmlWebpackPlugin({
+    title: 'Webpack Test',
+    template: './src/template.html'
+  })],
   module : {
     rules: [{
       test: /\.css$/,
